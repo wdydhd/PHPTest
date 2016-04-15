e11ff7d51183a455cb58e80daf4b1340ad2642e9
yanqi <yanqi@mac-PC-no-Mac-mini.local>
2016-04-15 18:00:41 +0900

    1

--- server/listview.php
+++ server/listview.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-if (empty($_POST["test"])){
+if (!empty($_POST["test"])){
 $test = $_POST["test"];
 $ret = array();
 for($x=0;$x<10;$x++){
@@ -10,4 +10,19 @@ echo json_encode($ret);
 
 }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
+$test = $_POST["test"];
+$ret = array();
+for($x=0;$x<10;$x++){
+$ret[]=$test.$x.$x.$x;
+}
+echo htmlspecialchars(json_encode($ret));
+
+}
+
+
+
+
+
 ?>
