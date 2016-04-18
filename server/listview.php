ba29afa52aafd6a94131a773acc2b6dfe0882270
yanqi <yanqi@mac-PC-no-Mac-mini.local>
2016-04-18 13:39:39 +0900

    a

--- server/listview.php
+++ server/listview.php
@@ -1,13 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 if (!empty($_POST["test"])){
-$test = $_POST["test"];
+$test=$_POST["test"];
+$section=$_POST["section"];
+$section=intval($section);
 $ret = array();
-for($x=0;$x<10;$x++){
-$ret[]=$test.$x.$x.$x;
+for($y=0;$y<$section;$y++){
+$sectionarr=array();
+for($x=0;$x<5;$x++){
+$sectionarr[]=$test.$x.$x.$x;
 }
+$ret["section is :".$y]=$sectionarr;
 echo json_encode($ret);
-
+}
 }
 }
 
