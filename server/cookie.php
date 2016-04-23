6cee709be28060002d8af265d855dac4714b9e99
Enki <wdydhd@icloud.com>
2016-04-23 19:21:30 +0800

    a

--- server/cookie.php
+++ server/cookie.php
@@ -1,10 +1,10 @@
-<?php 
+﻿<?php 
 setcookie("user", "Alex Porter", time()+3600);
 
-$value = "my cookie value";
+#$value = "my cookie value";
 
 // 发送一个 24 小时候过期的 cookie
-setrawcookie("TestCookie",$value, time()+3600*24);
+#setrawcookie("TestCookie",$value, time()+3600*24);
 ?>
 
 <html>
